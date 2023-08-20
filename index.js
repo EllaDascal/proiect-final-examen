69b43bdcfd2178c274edafbffeabbde502e41b94
EllaDascal <dascal.manuela@yahoo.com>
2023-08-20 21:57:56 +0300

    adjustments and modification

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const image = item.querySelector('img');
         const description = item.querySelector('.image-description');
         image.addEventListener('click', () => {
-            // Toggle the 'enlarged' class on the clicked image
+            
             image.classList.toggle('enlarged');
 
-            // Check if the clicked image is now enlarged
+     
             const isEnlarged = image.classList.contains('enlarged');
 
-            // If the clicked image is enlarged, loop through other images and make sure they are not enlarged
+            
             if (isEnlarged) {
                 images.forEach(otherImage => {
                     if (otherImage !== image && otherImage.classList.contains('enlarged')) {
